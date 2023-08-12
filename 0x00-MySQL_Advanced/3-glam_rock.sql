30e581cae03c1a2eb3e4366849d2322ec4b59c38
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-12 16:15:45 +0000

    task3 again

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,5 @@
 -- ranked by their longevity
 SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%";
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
