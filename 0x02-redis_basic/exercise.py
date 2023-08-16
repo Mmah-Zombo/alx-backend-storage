78e2875072d801fab6326fbbef6f55714ec85dd6
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-16 17:30:36 +0000

    added type annotaion

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ Main file
 
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -14,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data):
+    def store(self, data: Union[str, int, float]) -> str:
         """stores the data"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
