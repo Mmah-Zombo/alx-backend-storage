2b2b5267d0c28510a5d98b4a0593de8d845b0dab
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-16 17:33:59 +0000

    more variable annotation again

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the data"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
